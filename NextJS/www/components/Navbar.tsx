8bbeb9baf66a8ba1bd20a398b8e983427777ea1c
Heaknou <heaknou.h@bizsolution.com.kh>
2025-02-20 14:50:41 +0700

    update a to Link coz nextjs not use a

--- NextJS/www/components/Navbar.tsx
+++ NextJS/www/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import Link from 'next/link';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -14,29 +14,29 @@ const Navbar = () => {
     // </nav>
     <nav id="navmenu" className="navmenu">
         <ul>
-          <li><a href="/" className="active">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/resume">Resume</a></li>
-          <li><a href="/portfolio">Portfolio</a></li>
-          <li><a href="/service">Services</a></li>
+          <li><Link href="/" className="active">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/resume">Resume</Link></li>
+          <li><Link href="/portfolio">Portfolio</Link></li>
+          <li><Link href="/service">Services</Link></li>
           <li className="dropdown"><a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
-              <li><a href="#">Dropdown 1</a></li>
+              <li><Link href="#">Dropdown 1</Link></li>
               <li className="dropdown"><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
                 <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
+                  <li><Link href="#">Deep Dropdown 1</Link></li>
+                  <li><Link href="#">Deep Dropdown 2</Link></li>
+                  <li><Link href="#">Deep Dropdown 3</Link></li>
+                  <li><Link href="#">Deep Dropdown 4</Link></li>
+                  <li><Link href="#">Deep Dropdown 5</Link></li>
                 </ul>
               </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
+              <li><Link href="#">Dropdown 2</Link></li>
+              <li><Link href="#">Dropdown 3</Link></li>
+              <li><Link href="#">Dropdown 4</Link></li>
             </ul>
           </li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link href="#contact">Contact</Link></li>
         </ul>
         <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
